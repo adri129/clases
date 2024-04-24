e9f77320076900c8e9e55085b90d55519a0e49fb
adri129 <amolinaespigares@gmail.com>
2024-04-24 16:51:19 +0200

    modificación python

---
+++ proyecto/src/proyecto/python/main.py
@@ -0,0 +1,21 @@
+# declaración de variables
+numero = 5
+numeroReal = 3.2
+cadena = "hola mundo"
+ValorTrue = True
+
+# mostrar información
+
+print("hola mundo")
+
+print(numero)
+
+print("hola " + str(numero))
+
+valorEntrada = input()
+
+print("Has introducido " + valorEntrada)
+
+otroValor = int(input())
+
+print("Has introducido " + otroValor)
